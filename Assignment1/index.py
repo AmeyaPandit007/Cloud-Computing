60cf799b7610d6c51e6ef0a8098585acd0af18ac
AmeyaPandit007 <69378214+AmeyaPandit007@users.noreply.github.com>
2022-05-10 23:08:58 +0530

    Add files via upload

---
+++ Assignment1/index.py
@@ -0,0 +1,22 @@
+print("<h1>Hello, Ameya here..!</h1>")
+
+a = 12
+b = 27
+
+print("<hr>")
+print("\n*****Before Swapping*****\n")
+print("<br/>")
+
+print("a=",a)
+print("b=",b)
+print("<br/>")
+
+a = a+b
+b = a-b
+a = a-b
+print("<br/>")
+print("<hr>")
+print("\n*****After Swapping*****\n")
+print("<br/>")
+print("a=",a)
+print("b=",b)
